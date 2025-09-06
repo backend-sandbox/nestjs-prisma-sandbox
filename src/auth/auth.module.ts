1a1381314c94ea51e26a2bd9d639f61f2e6075e5
Andrew Kononenko <wastardy.k@gmail.com>
2025-09-06 14:25:55 +0300

    refactor(auth): update AuthModule to include JwtModule imports and exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Module } from '@nestjs/common';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
