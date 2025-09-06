d33f45dc8ff8f409fb731d2b8e053f2993ecec45
Andrew Kononenko <wastardy.k@gmail.com>
2025-09-06 13:19:08 +0300

    feat(auth): implement signup logic with user existence check and password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import bcryptjs from 'bcryptjs';
+import { ConflictException, Injectable } from '@nestjs/common';
+
+import { AuthDto } from './dtos';
+import { ERROR_CONSTANT } from 'src/constants';
+import { PrismaService } from '../prisma/prisma.service';
+import { mapUser } from 'src/mappers';
 
 @Injectable({})
 export class AuthService {
-  signup() {
-    return { hi: 'signup' };
+  constructor(private prismaService: PrismaService) {}
+
+  async signup(authDto: AuthDto) {
+    const { email, password } = authDto;
+
+    const existingUser = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      throw new ConflictException(ERROR_CONSTANT.EMAIL_ALREADY_IN_USE);
+    }
+
+    const passwordHash = bcryptjs.hashSync(password, 10);
+
+    const newUser = await this.prismaService.user.create({
+      data: {
+        email,
+        password: passwordHash,
+      },
+    });
+
+    const mappedUser = mapUser(newUser);
+    return mappedUser;
   }
 
-  signin() {
+  signin(authDto: AuthDto) {
     return { hi: 'signin' };
   }
 }
