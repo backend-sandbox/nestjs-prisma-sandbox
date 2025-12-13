24d0009ebe150b302e7d51cae8f99d398de08303
Wastardy <wastardy.k@gmail.com>
2025-12-14 01:53:34 +0200

    feat(interceptor): implement CurrentUserInterceptor to attach current user to request

---
+++ src/interceptors/current-user.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UserService } from '../user/user.service';
+import { RequestWithUser } from '../types';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private readonly userService: UserService) {}
+
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const { userId } = request.session ?? {};
+
+    if (userId) {
+      const currentUser = await this.userService.getUserById(userId);
+
+      if (currentUser) {
+        request.currentUser = currentUser;
+      }
+    }
+
+    return next.handle();
+  }
+}
